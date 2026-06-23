e702d3f3a2f8889400cc9ae92f435b012c17d7cd
Sergio Beltrán <sergiob.21@outlook.com>
2026-06-23 02:14:21 -0700

    Mi primer commit

---
+++ sender_stand_request.py
@@ -0,0 +1,26 @@
+import configuration
+import requests
+import data
+
+def post_products_kits(products_ids):
+    # Realiza una solicitud POST para buscar kits por productos.
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, # Concatenación de URL base y ruta.
+                         json=products_ids, # Datos a enviar en la solicitud.
+                         headers=data.headers) # Encabezados de solicitud.
+
+
+def post_new_user(body):
+    return requests.post(
+        configuration.URL_SERVICE +
+        configuration.CREATE_USER_PATH,
+        json=body,
+        headers=data.headers
+    )
+
+def get_users_table():
+
+    return requests.get(
+        configuration.URL_SERVICE +
+        configuration.USERS_TABLE_PATH
+    )
+
